365c1a3f205aa80f233ca356201e1d10de9ddde0
Wilted98 <vasy.kr7@gmail.com>
2022-11-01 22:30:49 +0200

    Codegen fix.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,3 @@
-export const SERVER_URL = 'http://192.168.1.181:4000/graphql';
+// export const SERVER_URL = 'https://haircut.vpopescu.com/graphql';
+
+export const SERVER_URL = 'http://192.168.1.181:6000/graphql';
