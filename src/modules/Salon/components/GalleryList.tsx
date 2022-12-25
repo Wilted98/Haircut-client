63c3f162a96ef0d25570ca2d5c29bd7dc449958d
vasile.popescu <vasile.popescu@clarisoft.com>
2022-12-25 16:59:11 +0200

    Image Gallery.

--- src/modules/Salon/components/GalleryList.tsx
+++ src/modules/Salon/components/GalleryList.tsx
@@ -1,44 +1,83 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {
+  View,
+  Image,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 import {useGetGalleryQuery} from '../../../generated/graphql';
+import ImageView from 'react-native-image-viewing';
 
 type GalleryListProps = {
   id: number;
 };
 
 const GalleryList: React.FC<GalleryListProps> = ({id}) => {
+  const [visible, setIsVisible] = React.useState<boolean>(false);
+  const [currentImageIndex, setImageIndex] = React.useState<number>(0);
   const [{data}] = useGetGalleryQuery({
     variables: {getGalleryId: id},
   });
 
-  React.useEffect(() => {
-    console.log(data);
-  }, [data]);
+  const width = Dimensions.get('window').width / 2;
+
+  const getAspectRatio = () => Math.random() * 0.35 + 1;
+
+  const renderItem = (photo: string, index: number, string: string) => {
+    console.log(`${string}__${index}`);
+    return (
+      <TouchableOpacity
+        key={`${string}__${index}`}
+        onPress={() => onSelect(index)}>
+        <Image
+          source={{uri: photo}}
+          style={{
+            height: width * getAspectRatio(),
+            width: width,
+            marginBottom: 10,
+            borderRadius: 10,
+          }}
+        />
+      </TouchableOpacity>
+    );
+  };
+
+  const onSelect = (index: number) => {
+    setImageIndex(index);
+    setIsVisible(true);
+  };
+
+  const images = data?.getGallery?.photos.map((item: string): {uri: string} => {
+    return {uri: item};
+  });
 
   return (
-    <View
-      style={{
-        width: '100%',
-        flexDirection: 'row',
-        height: '100%',
-        flexWrap: 'wrap',
-      }}>
-      {data?.getGallery?.photos.length! > 0 &&
-        data?.getGallery?.photos.map((photo, index) => {
-          return (
-            <Image
-              key={index}
-              style={{
-                width: '100%',
-                height: 200,
-                marginBottom: 20,
-                borderRadius: 20,
-              }}
-              source={{uri: photo}}
-            />
-          );
-        })}
-    </View>
+    <>
+      <ImageView
+        images={images!}
+        imageIndex={currentImageIndex}
+        visible={visible}
+        onRequestClose={() => setIsVisible(false)}
+      />
+      <ScrollView
+        style={{
+          height: '60%',
+        }}>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{marginRight: 5}}>
+            {data?.getGallery?.photos
+              .filter((_, i) => i % 2 === 0)
+              .map((item, index) => renderItem(item, index, 'left'))}
+          </View>
+          <View style={{marginLeft: 5}}>
+            {data?.getGallery?.photos
+              .filter((_, i) => i % 2 !== 0)
+              .map((item, index) => renderItem(item, index, 'right'))}
+          </View>
+        </View>
+      </ScrollView>
+    </>
   );
 };
 export default GalleryList;
